359a320f78e95980535ca3819829f33c29ab2f04
Taha Saleem <tstahasaleem@gmail.com>
2024-04-17 14:25:30 -0700

    js mini assignmentss

---
+++ app.js
@@ -0,0 +1,11 @@
+let myArr = [[2, 3, 4, 4, 4, 4], [5, 5], [5], [9, 8, 7]];
+
+//Here we are printing all values individually from this array dynamicly
+
+
+for (let i = 0; i < myArr.length; i++) {
+    
+    for (let j = 0; j < myArr[i].length; j++) {
+        console.log(myArr[i][j]);
+    }
+}
